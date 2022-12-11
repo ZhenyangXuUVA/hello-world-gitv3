5dcad20769f386c7e38f6d0c292e3537dcfde1a2
DeanXu-Upenn <deanxu@seas.upenn.edu>
2022-12-11 15:55:00 +0800

    Updated intro output message. Created scanner class for getting user input.

---
+++ HelloWorldGit/src/MyScanner.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Manages user input
+ * @author lenovo
+ *
+ */
+public class MyScanner {
+
+	private Scanner scanner;
+	
+	/**
+	 * Gets user input and returns it.
+	 * @return user input
+	 */
+	public String getInput() {
+		
+		this.scanner = new Scanner(System.in);
+		String input = this.scanner.nextLine();
+		this.scanner.close();
+		
+		return input;
+	}
+}
